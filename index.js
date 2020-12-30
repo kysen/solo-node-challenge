b095e745c1f45d60c9bdd9d65c19e883b6cad175
Kysen Jackman <kysenj@gmail.com>
2020-12-29 20:43:27 -0700

    done

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ const STARWARS_API = 'http://swapi.dev/api/'
 app.use(bodyParser.json())
 
 let allPlanets = []
+let planetsWithResidents = []
 let nextPlanetPage = `${STARWARS_API}planets/?page=1`
 
 let allPeople = []
 let nextPeoplePage = `${STARWARS_API}people/?page=1`
 
+// people {
 const getTenPeople = async () => {
   try {
     const planets = await axios.get(`${nextPeoplePage}`)
@@ -32,47 +34,46 @@ const getAllPeople = async () => {
   }
   return(allPeople)
 }
+let sortedPeople = []
+sortPeople = (people, sortParam) => {
+  let newArray = people
+  if (sortParam === "name") {
+    sortedPeople = newArray.sort((a, b) => (a.name > b.name) ? 1 : -1)
+  } else if (sortParam === "height") {
+    // sortedPeople = newArray.sort(function(a, b){return a.height - b.height})
+    sortedPeople = newArray.sort(function(a, b){return /[A-Za-z]/.test(a.height) - /[A-Za-z]/.test(b.height) || a.height - b.height})
 
-const getResident = async (res) => {
+  } else if (sortParam === "mass") {
+    sortedPeople = newArray.sort(function(a, b){return /[A-Za-z]/.test(a.mass) - /[A-Za-z]/.test(b.mass) || a.mass.replace(/,/g, '') - b.mass.replace(/,/g, '')})
+  } else {
+    sortedPeople = ["invalid search parameter"]
+  }
+}
+// } people
+
+// planets {
+const getResident = async (resURL) => {
   try {
-    const resident = await axios.get(res)
+    const resident = await axios.get(resURL)
     return resident.data.name
   } catch (error) {
-    console.log(error)
+    console.log(error, "getResident() Error")
   }
 }
 
-const getResidentArray = (littleArray, planet) => {
-  planet.residents.map(async(residentLink) => {
-    let resident = await getResident(residentLink)
-    littleArray = littleArray.concat(resident)
-    console.log(littleArray)
-    console.log(planet)
-    return {...planet, residents: littleArray}
-  }) 
+const mapPlanets = async () => {
+  return await allPlanets.map((planet, planetIndex) => {
+    planet.residents.map(async (resident, residentIndex) => {
+      allPlanets[planetIndex].residents[residentIndex] = await getResident(resident)
+    })
+  })
 }
- 
-const workPlanetData = (planets) => {
-  return (planets.map((planet) => {
-    let littleArray = []
-    return getResidentArray(littleArray, planet)
-
-  }))
-}
-// const workPlanetData = (planets) => {
-//   return (planets.map( (planet) => {
-//     let resArray = planet.residents.map(resident => {getResident(resident)})
-//     console.log(resArray)
-//     return {...planet, residents: resArray}
-//   }))
-// }
 
 
 const getTenPlanets = async () => {
   try {
     let planets = await axios.get(`${nextPlanetPage}`)
-    let planetData = workPlanetData(planets.data.results)
-
+    let planetData = planets.data.results
     nextPlanetPage = planets.data.next
     allPlanets = allPlanets.concat(planetData)
   } catch (error) {
@@ -82,31 +83,19 @@ const getTenPlanets = async () => {
 
 const getAllPlanets = async () => {
   await getTenPlanets()
-  // while (typeof nextPlanetPage === 'string' ) {
-  //   await getTenPlanets()
-  // }
+  while (typeof nextPlanetPage === 'string' ) {
+    await getTenPlanets()
+  }
+  await mapPlanets()
 
   return(allPlanets)
 }
+// } planets
 
 
 getAllPlanets()
 getAllPeople()
 
-let sortedPeople = []
-sortPeople = (people, sortParam) => {
-  // console.log(people, 'sort')
-  let newArray = people
-  if (sortParam === "name") {
-    sortedPeople = newArray.sort((a, b) => (a.name > b.name) ? 1 : -1)
-  } else if (sortParam === "height") {
-    sortedPeople = newArray.sort(function(a, b){return a.height - b.height})
-  } else if (sortParam === "mass") {
-    sortedPeople = newArray.sort(function(a, b){return a.mass - b.mass})
-  } else {
-    sortedPeople = ["invalid search parameter"]
-  }
-}
 
 
 app.get('/planets', (req, res) => {
@@ -118,7 +107,6 @@ app.get('/people', (req, res) => {
 })
 
 app.get('/people/:sortBy', (req, res) => {
-  console.log(req.params.sortBy)
   sortPeople(allPeople, req.params.sortBy)
   res.send(sortedPeople)
 })
